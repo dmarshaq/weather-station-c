2f9410d63f03fca15cd84a5711c027dfbc9e436e
Samuel Sharivker <sjsharivker@gmail.com>
2025-10-05 19:24:02 -0400

    Created function to give approximations for data pointers based on real climate data.

--- src/main/devices.c
+++ src/main/devices.c
@@ -2,6 +2,7 @@
 
 
 int devices_init(Devices_Info *devices_info) {
+    srand(time(NULL));
     FILE *file = fopen("data.csv", "a");
 
     if(file==NULL) {
@@ -13,6 +14,25 @@ int devices_init(Devices_Info *devices_info) {
 }
 
 int devices_collect_data(Data_Point *data_point) {
-    // @Incomplete.
+    if(data_point==NULL) {
+        return -1
+    }
+
+    //Used to approximate values
+    //https://weatherspark.com/y/20372/Average-Weather-in-Buffalo-New-York-United-States-Year-Round
+
+    data_point->temperature = 64.0f + ((float)rand() / (float)RAND_MAX * (79.0f - 64.0f));
+    data_point->humidity = 0.0f + ((float)rand() / (float)RAND_MAX * (31.0f - 0.0f));
+    data_point->wind_speed = 6.0f + ((float)rand() / (float)RAND_MAX * (11.9f - 6.0f));
+    data_point->wind_direction = 0.0f + ((float)rand() / (float)RAND_MAX * (360.0f - 0.0f));
+    data_point->pressure = 979.35f + ((float)rand() / (float)RAND_MAX * (1048.75f - 979.35f));
+    data_point->precipitation = 30.48f + ((float)rand() / (float)RAND_MAX * (73.66f - 30.48f));
+    data_point->uv_index = 0.0f + ((float)rand() / (float)RAND_MAX * (15.0f - 0.0f));
+
+    time_t t = time(NULL);
+    struct tm *localTime = localtime(&t);
+    if(localTime!=NULL) {
+        data_point->timestamp = *lt;
+    }
     return 0;
 }
